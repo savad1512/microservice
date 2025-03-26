5d971aae431eb54eb0d847eaab5eb9aa032101e6
savad1512 <muhdsavadkt@gmail.com>
2025-03-26 16:39:20 +0530

    secont

---
+++ order-service/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Query, HTTPException # type: ignore
+import psycopg2 # type: ignore
+
+app = FastAPI()
+
+# Database connection
+conn = psycopg2.connect("dbname=order_db user=postgres password=secret host=db")
+cursor = conn.cursor()
+
+@app.get("/orders/")
+def get_order(
+    order_id: int | None = Query(None, description="Order ID (required if customer_name is not provided)"),
+    customer_name: str | None = Query(None, description="Customer name (required if order_id is not provided)")
+): 
+    if order_id is None and customer_name is None:
+        raise HTTPException(status_code=400, detail="Either order_id or customer_name must be provided.")   
+    cursor.execute(
+        "SELECT order_id, order_name, customer_name, order_date, total_amount FROM orders WHERE order_id = %s or customer_name = %s",
+        (order_id,customer_name)
+    )
+    order = cursor.fetchone()
+    if order:
+        return {
+            "order_id": order[0],
+            "order_name": order[1],
+            "customer_name": order[2],
+            "order_date": order[3],
+            "total_amount": order[4],
+        }
+    raise HTTPException(status_code=404, detail="Order not found")
---
+++ user-service/main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, Query, HTTPException # type: ignore
+
+import psycopg2 # type: ignore
+
+app = FastAPI()
+
+# Database connection (PostgreSQL)
+conn = psycopg2.connect("dbname=user_db user=postgres password=secret host=db")
+cursor = conn.cursor()
+
+@app.get("/users/")
+def get_users(
+    user_id: int | None = Query(None, description="User ID (required if user_name is not provided)"),
+    user_name: str | None = Query(None, description="user_name (required if user_id is not provided)")
+):
+    if user_id is None and user_name is None:
+        raise HTTPException(status_code=400, detail="Either user_id or user_name must be provided.")
+    cursor.execute(
+        "SELECT user_id, user_name, mobile_number FROM users WHERE user_id = %s or user_name = %s",
+        (user_id,user_name)
+    )
+    users = cursor.fetchone()
+    if users:
+        return {
+            "user_id" : users[0],
+            "user_name" : users[1],
+            "mobile_number" : users[2],
+        }
+    raise HTTPException(status_code=404, detail="users not found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI
+# import psycopg2
+
+# app = FastAPI()
+
+# # Database connection (PostgreSQL)
+# conn = psycopg2.connect("dbname=user_db user=postgres password=secret host=db")
+# cursor = conn.cursor()
+
+# @app.get("/users/{user_id}")
+# def get_user(user_id: int):
+#     cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+#     user = cursor.fetchone()
+#     return {"user": user}
